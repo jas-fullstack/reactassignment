aa249fb294ec1aa156985e2036feb9f1e8a089dd
sanjeevkumar <sanjeevkumar@smartdatainc.net>
2021-05-08 15:23:00 +0530

    Updates

---
+++ precache-manifest.c11d23721afc1dd7b5ee2091dc8a602e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2757de49a574132b3fcd85e3efcb7649",
+    "url": "/reactAssignment/index.html"
+  },
+  {
+    "revision": "4983aa0e797144d2762e",
+    "url": "/reactAssignment/static/css/2.0f8e233a.chunk.css"
+  },
+  {
+    "revision": "0b03aaa7595b7a56fff4",
+    "url": "/reactAssignment/static/css/main.b6d760ea.chunk.css"
+  },
+  {
+    "revision": "4983aa0e797144d2762e",
+    "url": "/reactAssignment/static/js/2.c28a94bf.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/reactAssignment/static/js/2.c28a94bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b03aaa7595b7a56fff4",
+    "url": "/reactAssignment/static/js/main.44211939.chunk.js"
+  },
+  {
+    "revision": "36517912bf604e4cf8c7",
+    "url": "/reactAssignment/static/js/runtime-main.a49099d1.js"
+  }
+]);
